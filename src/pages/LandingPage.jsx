2dfaac6dc51a6729f9bba33eae29956d203bb148
Abhaysoft-inc <av385735@gmail.com>
2025-11-25 12:24:52 +0530

    added more sections

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import HeroSection from '../components/HeroSection.jsx'
 import AboutSection from '../components/AboutSection.jsx'
 import Navbar from '../components/Navbar.jsx'
-
+import Timeline from '../components/Timeline.jsx'
+import Faqs from '../components/Faqs.jsx'
+import Footer from '../components/Footer.jsx'
+import Tracks from '../components/Tracks.jsx'
 
 const LandingPage = () => {
     return (
@@ -10,6 +13,11 @@ const LandingPage = () => {
             <Navbar />
             <HeroSection />
             <AboutSection />
+            <Tracks />
+            <Timeline />
+            <Faqs />
+            <Footer />
+
         </div>
     )
 }
